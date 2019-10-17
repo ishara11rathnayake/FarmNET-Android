2b5666d23ab9abaf421c5338641946552c687b3c
ishara11rathnayake <ishara11rathnayake@gmail.com>
2019-10-18 00:17:52 +0530

    refactor Main Activity

--- app/src/main/java/com/industrialmaster/farmnet/views/activities/MainActivity.java
+++ app/src/main/java/com/industrialmaster/farmnet/views/activities/MainActivity.java
@@ -3,11 +3,9 @@ package com.industrialmaster.farmnet.views.activities;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -15,7 +13,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.Gravity;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -47,17 +44,13 @@ public class MainActivity extends BaseActivity implements FarmnetHomeView {
 
     String mUserType;
 
-    private DrawerLayout mDrawerlayout;
-    private ImageView imgv_drawer_toggle;
-    private ImageView img_btn_new_question;
-    private ImageView mLogoutImageView;
-    private TextView txt_header_topic;
-
-    private ImageButton img_btn_create_new_deal, img_btn_new_article;
+    private ImageView mNewQuestionImageView;
+    private TextView mHeaderTopicTextView;
+    private ImageButton mCreateNewDealImageButton;
+    private ImageButton mNewArticleImageButton;
     private ImageButton mNewAdsImageButton;
-
-    BottomNavigationView bottom_navigation_view;
-    NavigationView drawer_navigation_view;
+    BottomNavigationView mBottomNavigationView;
+    NavigationView mDrawerNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,61 +58,48 @@ public class MainActivity extends BaseActivity implements FarmnetHomeView {
 
         authPresenter = new AuthPresenterImpl(this, MainActivity.this );
 
-        authPresenter.doCheckAlreadyLogin();
-
         mUserType = getSharedPreferences("FarmnetPrefsFile", Context.MODE_PRIVATE)
                 .getString(FarmnetConstants.USER_TYPE, "");
 
-        mLogoutImageView = findViewById(R.id.imgvlogout);
-        txt_header_topic = findViewById(R.id.txtheadertopic);
-        img_btn_new_question =findViewById(R.id.img_btn_new_question);
+        authPresenter.doCheckAlreadyLogin();
+
+        ImageView mLogoutImageView = findViewById(R.id.imgvlogout);
+        mHeaderTopicTextView = findViewById(R.id.txtheadertopic);
+        mNewQuestionImageView =findViewById(R.id.img_btn_new_question);
         mNewAdsImageButton = findViewById(R.id.img_btn_new_ads);
-        img_btn_create_new_deal = findViewById(R.id.img_btn_new_deal);
-        img_btn_new_article = findViewById(R.id.img_btn_new_article);
+        mCreateNewDealImageButton = findViewById(R.id.img_btn_new_deal);
+        mNewArticleImageButton = findViewById(R.id.img_btn_new_article);
 
         //click on create new deal button
-        img_btn_create_new_deal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, CreateNewDealActivity.class));
-            }
-        });
+        mCreateNewDealImageButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, CreateNewDealActivity.class)));
 
-        img_btn_new_article.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, CreateNewArticleActivity.class));
-            }
-        });
+        mNewArticleImageButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, CreateNewArticleActivity.class)));
 
-        mNewAdsImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, CreateNewAdvertisementActivity.class));
-            }
-        });
+        mNewAdsImageButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, CreateNewAdvertisementActivity.class)));
 
         //click on logout button
-        mLogoutImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showLogoutAlert(ErrorMessageHelper.LOGOUT_CONFIRMATION);
-            }
-        });
+        mLogoutImageView.setOnClickListener(v -> showLogoutAlert(ErrorMessageHelper.LOGOUT_CONFIRMATION));
 
     }
 
-    //set fragment when clicking bottom navigation item
+    /**
+     * set fragment when clicking bottom navigation item
+     * @param fragment the fragment
+     */
     public void setFragment(Fragment fragment){
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.frame, fragment);
         fragmentTransaction.commit();
     }
 
+    /**
+     * set main activity content after checking already logged user or not
+     */
     public void setMainActivityContent() {
 
         setContentView(R.layout.content_main);
 
+        //Firebase push notification
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // Create channel to show notifications.
             String channelId  = getString(R.string.default_notification_channel_id);
@@ -137,137 +117,149 @@ public class MainActivity extends BaseActivity implements FarmnetHomeView {
             }
         }
 
-        mDrawerlayout = findViewById(R.id.drawernavigation);
-        imgv_drawer_toggle = findViewById(R.id.imgvdrawertoggle);
+        DrawerLayout mDrawerlayout = findViewById(R.id.drawernavigation);
+        ImageView mDrawerToggleImageView = findViewById(R.id.imgvdrawertoggle);
 
-        bottom_navigation_view = findViewById(R.id.bottom_nav);
+        mBottomNavigationView = findViewById(R.id.bottom_nav);
         dealsFragment = new DealsFragment();
         qandAFragment = new QandAFragment();
         advertisementFragment = new AdvertisementFragment();
         notificationFragment = new NotificationFragment();
         articleFragment = new ArticleFragment();
 
-        drawer_navigation_view = findViewById(R.id.drawer_nav);
+        mDrawerNavigationView = findViewById(R.id.drawer_nav);
 
-        imgv_drawer_toggle.setOnClickListener(v -> mDrawerlayout.openDrawer(Gravity.LEFT));
+        mDrawerToggleImageView.setOnClickListener(v -> mDrawerlayout.openDrawer(Gravity.START));
 
-        bottom_navigation_view.setSelectedItemId(R.id.deals);
+        mBottomNavigationView.setSelectedItemId(R.id.deals);
         setFragment(dealsFragment);
 
         //bottom navigation item selecting (change fragments)
-        bottom_navigation_view.setOnNavigationItemSelectedListener(menuItem -> {
+        mBottomNavigationView.setOnNavigationItemSelectedListener(menuItem -> {
             int id = menuItem.getItemId();
-            if(id == R.id.deals){
-                setFragment(dealsFragment);
-                txt_header_topic.setText(getResources().getString(R.string.crop_deals));
-                img_btn_new_question.setVisibility(View.GONE);
-                if(mUserType.equals(FarmnetConstants.UserTypes.FARMER)){
-                    img_btn_create_new_deal.setVisibility(View.VISIBLE);
-                } else {
-                    img_btn_create_new_deal.setVisibility(View.INVISIBLE);
-                }
-                img_btn_new_article.setVisibility(View.GONE);
-                mNewAdsImageButton.setVisibility(View.GONE);
-                return true;
-            } else if(id == R.id.notification){
-                setFragment(notificationFragment);
-                txt_header_topic.setText(getResources().getString(R.string.notification));
-                img_btn_create_new_deal.setVisibility(View.INVISIBLE);
-                img_btn_new_question.setVisibility(View.GONE);
-                img_btn_new_article.setVisibility(View.GONE);
-                mNewAdsImageButton.setVisibility(View.GONE);
-                return true;
-            } else if(id == R.id.question){
-                setFragment(qandAFragment);
-                txt_header_topic.setText("Q & A");
-                img_btn_create_new_deal.setVisibility(View.INVISIBLE);
-                img_btn_new_question.setVisibility(View.INVISIBLE);
-                img_btn_new_article.setVisibility(View.INVISIBLE);
-                mNewAdsImageButton.setVisibility(View.INVISIBLE);
-                return true;
-            } else if(id == R.id.articles){
-                setFragment(articleFragment);
-                txt_header_topic.setText(getResources().getString(R.string.articles));
-                img_btn_create_new_deal.setVisibility(View.GONE);
-                img_btn_new_question.setVisibility(View.GONE);
-                if(mUserType.equals(FarmnetConstants.UserTypes.KNOWLEDGE_PROVIDER)){
-                    img_btn_new_article.setVisibility(View.VISIBLE);
-                } else {
-                    img_btn_new_article.setVisibility(View.INVISIBLE);
-                }
-                mNewAdsImageButton.setVisibility(View.GONE);
-                return true;
-            } else if(id == R.id.advertisements){
-                setFragment(advertisementFragment);
-                txt_header_topic.setText(getResources().getString(R.string.ads));
-                if(mUserType.equals(FarmnetConstants.UserTypes.SERVICE_PROVIDER)){
-                    mNewAdsImageButton.setVisibility(View.VISIBLE);
-                } else {
-                    mNewAdsImageButton.setVisibility(View.INVISIBLE);
-                }
-                img_btn_create_new_deal.setVisibility(View.GONE);
-                img_btn_new_article.setVisibility(View.GONE);
-                img_btn_new_question.setVisibility(View.GONE);
-                return true;
-            }
-            return false;
+            return onBottomNavigationItemSelected(id);
         });
 
         //navigation drawer item selecting
-        drawer_navigation_view.setNavigationItemSelectedListener(menuItem -> {
+        mDrawerNavigationView.setNavigationItemSelectedListener(menuItem -> {
             int id = menuItem.getItemId();
-            if(id == R.id.profile){
-                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
-            } else if(id == R.id.logout){
-                showLogoutAlert(ErrorMessageHelper.LOGOUT_CONFIRMATION);
-            } else if(id == R.id.home){
-                finish();
-                startActivity(getIntent());
-            } else  if(id == R.id.my_questions){
-                startActivity(new Intent(MainActivity.this, MyQuestionActivity.class));
-            } else if(id == R.id.change_password){
-                startActivity(new Intent(MainActivity.this, ChangePasswordActivity.class));
-            }
-            return false;
+            return onDrawerNavigationItemSelected(id);
         });
 
     }
 
+    /**
+     * on drawer navigation item selected
+     * @param id drawer navigation item id
+     * @return boolean
+     */
+    private boolean onDrawerNavigationItemSelected(int id){
+        if(id == R.id.profile){
+            startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+        } else if(id == R.id.logout){
+            showLogoutAlert(ErrorMessageHelper.LOGOUT_CONFIRMATION);
+        } else if(id == R.id.home){
+            finish();
+            startActivity(getIntent());
+        } else  if(id == R.id.my_questions){
+            startActivity(new Intent(MainActivity.this, MyQuestionActivity.class));
+        } else if(id == R.id.change_password){
+            startActivity(new Intent(MainActivity.this, ChangePasswordActivity.class));
+        }
+        return false;
+    }
+
+    /**
+     * on bottom navigation item selected
+     * @param id bottom navigation item id
+     * @return boolean
+     */
+    private boolean onBottomNavigationItemSelected(int id){
+        if(id == R.id.deals){
+            setFragment(dealsFragment);
+            mHeaderTopicTextView.setText(getResources().getString(R.string.crop_deals));
+            mNewQuestionImageView.setVisibility(View.GONE);
+            if(mUserType.equals(FarmnetConstants.UserTypes.FARMER)){
+                mCreateNewDealImageButton.setVisibility(View.VISIBLE);
+            } else {
+                mCreateNewDealImageButton.setVisibility(View.INVISIBLE);
+            }
+            mNewArticleImageButton.setVisibility(View.GONE);
+            mNewAdsImageButton.setVisibility(View.GONE);
+            return true;
+        } else if(id == R.id.notification){
+            setFragment(notificationFragment);
+            mHeaderTopicTextView.setText(getResources().getString(R.string.notification));
+            mCreateNewDealImageButton.setVisibility(View.INVISIBLE);
+            mNewQuestionImageView.setVisibility(View.GONE);
+            mNewArticleImageButton.setVisibility(View.GONE);
+            mNewAdsImageButton.setVisibility(View.GONE);
+            return true;
+        } else if(id == R.id.question){
+            setFragment(qandAFragment);
+            mHeaderTopicTextView.setText("Q & A");
+            mCreateNewDealImageButton.setVisibility(View.INVISIBLE);
+            mNewQuestionImageView.setVisibility(View.INVISIBLE);
+            mNewArticleImageButton.setVisibility(View.INVISIBLE);
+            mNewAdsImageButton.setVisibility(View.INVISIBLE);
+            return true;
+        } else if(id == R.id.articles){
+            setFragment(articleFragment);
+            mHeaderTopicTextView.setText(getResources().getString(R.string.articles));
+            mCreateNewDealImageButton.setVisibility(View.GONE);
+            mNewQuestionImageView.setVisibility(View.GONE);
+            if(mUserType.equals(FarmnetConstants.UserTypes.KNOWLEDGE_PROVIDER)){
+                mNewArticleImageButton.setVisibility(View.VISIBLE);
+            } else {
+                mNewArticleImageButton.setVisibility(View.INVISIBLE);
+            }
+            mNewAdsImageButton.setVisibility(View.GONE);
+            return true;
+        } else if(id == R.id.advertisements){
+            setFragment(advertisementFragment);
+            mHeaderTopicTextView.setText(getResources().getString(R.string.ads));
+            if(mUserType.equals(FarmnetConstants.UserTypes.SERVICE_PROVIDER)){
+                mNewAdsImageButton.setVisibility(View.VISIBLE);
+            } else {
+                mNewAdsImageButton.setVisibility(View.INVISIBLE);
+            }
+            mCreateNewDealImageButton.setVisibility(View.GONE);
+            mNewArticleImageButton.setVisibility(View.GONE);
+            mNewQuestionImageView.setVisibility(View.GONE);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * show log out alert
+     * @param message logout alert message
+     */
     public void showLogoutAlert(String message) {
-        showAlertDialog("Logout", message, true, FarmnetConstants.LOGOUT, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                authPresenter.doLogout();
-            }
-        }, FarmnetConstants.CANCEL, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
+        showAlertDialog("Logout", message, true,
+                FarmnetConstants.LOGOUT, (dialog, which) -> authPresenter.doLogout(),
+                FarmnetConstants.CANCEL, (dialog, which) -> dialog.dismiss());
     }
 
-    //check user is a new user or already logged user or logout user
+    /**
+     * check user is a new user or already logged user or logout user
+     */
     @Override
     public void setStarterScreen(String screenName) {
-        if(screenName.equals(FarmnetConstants.CheckUserLogin.NEW_USER)){
+        if (FarmnetConstants.CheckUserLogin.NEW_USER.equals(screenName)) {
             startActivity(new Intent(MainActivity.this, StartActivity.class));
             finish();
-        } else if(screenName.equals(FarmnetConstants.CheckUserLogin.LOGOUT_USER)){
+        } else if (FarmnetConstants.CheckUserLogin.LOGOUT_USER.equals(screenName)) {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
             finish();
-        } else if(screenName.equals(FarmnetConstants.CheckUserLogin.ALREADY_LOGGED_USER)){
+        } else if (FarmnetConstants.CheckUserLogin.ALREADY_LOGGED_USER.equals(screenName)) {
             setMainActivityContent();
         }
-
     }
 
     @Override
     public void showMessage(String message) {
-        /**
-         * nothing to do with this function
-         * need to remove
-         * **/
+
     }
 
 }
